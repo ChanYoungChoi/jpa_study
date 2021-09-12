a6c1db789acf7554d486feebf000838d2acd1afa
Choi Chan Young <rsefa5@gmail.com>
2021-09-12 17:47:20 +0900

    프로젝트 환경설정

--- src/test/java/com/example/demo/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/demo/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import com.example.demo.entity.Member;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,19 +13,22 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @Transactional
 @Rollback(false)
-class MemberJpaRepositoryTest {
+class MemberRepositoryTest {
 
     @Autowired
-    MemberJpaRepository memberJpaRepository;
+    MemberRepository memberRepository;
 
     @Test
-    public void testMember(){
-        Member member = new Member("memberA");
-        Member savedMember = memberJpaRepository.save(member);
-
-        Member findMember = memberJpaRepository.find(savedMember.getId());
+    public void testMember() {
+        Member member = new Member("mmeberA");
+        Member savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+
     }
+
 
 }
